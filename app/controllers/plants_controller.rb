04a15d4658c56e9933e961c20b22c7d700f1a2ed
Elijah Silverman <elijahmsilverman@gmail.com>
2021-12-13 10:58:53 -0500

    commit tree 441ac0308c1d794ca4ca3886976946398b7691d1

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,15 @@
+class PlantsController < ApplicationController
+
+    def index
+        render json: Plant.all
+    end
+
+    def show 
+        render json: Plant.find(params[:id])
+    end
+
+    def create
+        render json: Plant.create(params.permit(:name, :image, :price)), status: :created
+    end
+
+end
